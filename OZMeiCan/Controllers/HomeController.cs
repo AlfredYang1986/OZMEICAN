a3f142dd7a3ba3ebce2ccf27ebec71c702b39f0c
AlfredYang1986 <yangyuanpig@gmail.com>
2014-11-23 01:48:49 +1100

    nav menu

--- OZMeiCan/Controllers/HomeController.cs
+++ OZMeiCan/Controllers/HomeController.cs
@@ -3,16 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OZMeiCan.Controllers
 {
     public class HomeController : Controller
     {
+        public JsonResult getJsonData()
+        {
+            JArray provences = new JArray();
+            for (int p_index = 0; p_index < 9; ++p_index)
+            {
+                dynamic provence = new JObject();
+                provence.shot = @"P";
+                provence.name = String.Format(@"provence{0}", p_index);
+
+                JArray cities = new JArray();
+                for (int c_index = 0; c_index < 9; ++c_index)
+                {
+                    dynamic city = new JObject();
+                    city.shot = @"C";
+                    city.name = String.Format("city{0}", c_index);
+
+                    JArray distrects = new JArray();
+                    for (int index = 0; index < 9; ++index)
+                    {
+                        dynamic dis = new JObject();
+                        dis.shot = @"D";
+                        dis.name = String.Format("distrect{0}", index);
+                        distrects.Add(dis);
+                    }
+                    city.subs = distrects;
+                    cities.Add(city);
+                }
+                provence.subs = cities;
+                provences.Add(provence);
+            }
+
+            return Json(JsonConvert.SerializeObject(provences), JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
+            return View("Index");
         }
     }
 }
--- OZMeiCan/Scripts/CityNav/CityNav.js
+++ OZMeiCan/Scripts/CityNav/CityNav.js
@@ -1,4 +1,90 @@
 ﻿
-function CityNav(jsonData) {
-    this.
+function CityNav(callBack) {
+    this.queryData();
+    this.cb = callBack;
+}
+
+CityNav.prototype.queryData = function () {
+    var tmp = this;
+    $.ajax({
+        url: '/Home/getJsonData',//'@Url.Action("Search", "MWHome")',
+        dataType: 'json',
+        contentType: 'application/json, charset=utf-8',
+        cache: false,
+        //beforeSend: function () {
+        //    Loading();
+        //},
+        //complete: function () {
+        //    hiddenLoading();
+        //},
+        success: function (data) {
+            tmp.data = eval(data);
+            tmp.cb();
+        },
+        error: function (xhr, status, error) {
+            alert(error);
+        }
+    });
+}
+
+CityNav.prototype.getProvencesCount = function () {
+    return this.data.length;
+}
+
+CityNav.prototype.getCitiesCountByProvence = function (name) {
+    var reVal = 0;
+    $.each(this.data, function (index, item) {
+        if (item.name == name) reVal = item.subs.length;
+    });
+    return reVal;
+}
+
+CityNav.prototype.getDistrectCountByProAndCity = function (p, c) {
+    var reVal = 0;
+
+    $.each(this.data, function (index, item) {
+        if (item.name == p) {
+            $.each(item.subs, function (ic, item_c) {
+                if (item_c.name == c) reVal = item_c.subs.length;
+            });
+        }
+    });
+
+    return reVal;
+}
+
+CityNav.prototype.getProvencesArray = function () {
+    var reVal = new Array();
+    $.each(this.data, function (index, item) {
+        reVal.push(item.name);
+    });
+    return reVal;
+}
+
+CityNav.prototype.getCitiesArrayByProvence = function (name) {
+    var reVal = new Array();
+    $.each(this.data, function (index, item) {
+        if (item.name == name) {
+            $.each(item.subs, function (i, it) {
+                reVal.push(it.name);
+            });
+        }
+    });
+    return reVal;
+}
+
+CityNav.prototype.getDistrectArrayByProAndCity = function (p, c) {
+    var reVal = new Array();
+    $.each(this.data, function (index, item) {
+        if (item.name == p) {
+            $.each(item.subs, function (i, it) {
+                if (it.name == c) {
+                    $.each(it.subs, function (id, itc) {
+                        reVal.push(itc.name);
+                    });
+                }
+            });
+        }
+    });
+    return reVal;
 }
