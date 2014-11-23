1ad403be6c8fe30eec52dbb8b118127b5f550c0a
AlfredYang1986 <yangyuanpig@gmail.com>
2014-11-23 23:33:01 +1100

    second page

--- OZMeiCan/Scripts/RtList/RtList.js
+++ OZMeiCan/Scripts/RtList/RtList.js
@@ -3,7 +3,7 @@ function RtList() {
 
 }
 
-RtList.prototype.getRTContent = function () {
+RtList.prototype.createRTContent = function () {
     var self = this;
 
     $.ajax({
@@ -19,7 +19,8 @@ RtList.prototype.getRTContent = function () {
         //    hiddenLoading();
         //},
         success: function (data) {
-            alert(data)
+            self.rt = eval(data);
+            self.initRTView();
         },
         error: function (xhr, status, error) {
             alert(error);
@@ -27,14 +28,30 @@ RtList.prototype.getRTContent = function () {
     });
 }
 
-RtList.prototype.getDishContent = function (name, cor) {
+RtList.prototype.initRTView = function () {
+    var self = this;
+
+    $('#rtList').children().remove();
+
+    $.each(this.rt, function (index, item) {
+        var element = $("<div class='rtBtn' style='width: 100%; height: 60px; border: 1px solid black'></div>").appendTo($('#rtList'));
+        element.html(item.name);
+
+        element.click(function () {
+            self.createDishContent("test", "test");
+            self.inDishView();
+        });
+    });
+}
+
+RtList.prototype.createDishContent = function (name, cor) {
     var self = this;
 
     $.ajax({
         url: '/Home/getDishContent',
         dataType: 'json',
         contentType: 'application/json, charset=utf-8',
-        data: { "name": "TestAlfred", "cor": "AlfredTest" },
+        data: { "name": name, "cor": cor },
         cache: false,
         //beforeSend: function () {
         //    Loading();
@@ -43,10 +60,26 @@ RtList.prototype.getDishContent = function (name, cor) {
         //    hiddenLoading();
         //},
         success: function (data) {
-            alert(data)
+            self.ds = eval(data);
+            self.initDishView();
         },
         error: function (xhr, status, error) {
             alert(error);
         }
+    });
+}
+
+RtList.prototype.initDishView = function () {
+    var self = this;
+
+    $('#dishList').children().remove();
+
+    $.each(this.ds, function (index, item) {
+        var element = $("<div class='dishBtn' style='width: 100%; height: 60px; border: 1px solid black'></div>").appendTo($('#dishList'));
+        element.html(item.name);
+
+        element.click(function () {
+            alert("dish click");
+        });
     });
 }
