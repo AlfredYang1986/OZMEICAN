77d5a2daf69ae402f0e532393e8dbd82d99eaa0a
AlfredYang1986 <yangyuanpig@gmail.com>
2014-11-30 17:52:48 +1100

    first map version

--- OZMeiCan/Scripts/CityNav/CityNav.js
+++ OZMeiCan/Scripts/CityNav/CityNav.js
@@ -193,6 +193,23 @@ CityNav.prototype.createListButton = function (array, level) {
             if (parseInt(element.attr('data-level')) != 4) {
                 self.createNavButton(tmp.textContent, level);
                 self.listAllBtns();
+            } else {
+                /**
+                 * 5. 跳到地图
+                 */
+                var w = $('#cityNav').width();
+                var hNav = $('#cityNav').height();
+                var hCon = $('#cities').height();
+                $('#cityNav').hide();
+                $('#cities').hide();
+                $('#cityMap').show();
+                $('#cityMap').css({ "width": w + "px", "height": hNav + hCon + "px" });
+
+                var map = new BMap.Map("cityMap");    // 创建Map实例
+                map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+                map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+                map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+                map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
             }
         });
     });
