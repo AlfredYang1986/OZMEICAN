4e140cd9de05d60d643a022973968eef66891edb
AlfredYang1986 <yangyuanpig@gmail.com>
2014-12-01 18:06:43 +1100

    Change baidu to Google map

---
+++ OZMeiCan/Entity/IGoDeliver.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\yy\Desktop\OZMeiCan\OZMeiCan\Entity\IGoDeliver.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
--- OZMeiCan/Scripts/CityNav/CityNav.js
+++ OZMeiCan/Scripts/CityNav/CityNav.js
@@ -205,11 +205,26 @@ CityNav.prototype.createListButton = function (array, level) {
                 $('#cityMap').show();
                 $('#cityMap').css({ "width": w + "px", "height": hNav + hCon + "px" });
 
-                var map = new BMap.Map("cityMap");    // 创建Map实例
-                map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
-                map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-                map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-                map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+                //var map = new BMap.Map("cityMap");    // 创建Map实例
+                //map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+                //map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+                //map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+                //map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
+                var mapProp = {
+                    center: new google.maps.LatLng(51.508742, -0.120850),
+                    zoom: 7,
+                    panControl: true,
+                    zoomControl: true,
+                    mapTypeControl: true,
+                    scaleControl: true,
+                    streetViewControl: true,
+                    overviewMapControl: true,
+                    rotateControl: true,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                var map = new google.maps.Map(document.getElementById("cityMap")
+                  , mapProp);
             }
         });
     });
